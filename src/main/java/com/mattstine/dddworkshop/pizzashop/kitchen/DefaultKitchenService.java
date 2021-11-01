7b99ef0b475231ca753cbdae396f80d89a23f621
Nilesh Gule <vn_nilesh@yahoo.com>
2021-11-01 22:07:39 +0800

    lab 6 first part solution with event subscriptions

--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/DefaultKitchenService.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/DefaultKitchenService.java
@@ -1,6 +1,7 @@
 package com.mattstine.dddworkshop.pizzashop.kitchen;
 
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.EventLog;
+import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Topic;
 import com.mattstine.dddworkshop.pizzashop.ordering.OnlineOrderRef;
 import com.mattstine.dddworkshop.pizzashop.ordering.OrderingService;
 import lombok.Value;
@@ -19,38 +20,56 @@ final class DefaultKitchenService implements KitchenService {
 		this.eventLog = eventLog;
 		this.pizzaRepository = pizzaRepository;
 		this.orderingService = orderingService;
+
+		eventLog.subscribe(new Topic("kitchen_orders"), e ->{
+
+		});
+
+		eventLog.subscribe(new Topic("ordering"), e -> {
+
+		});
+
+		eventLog.subscribe(new Topic("pizzas"), e -> {
+
+		});
 	}
 
 	@Override
 	public void startOrderPrep(KitchenOrderRef kitchenOrderRef) {
+		KitchenOrder kitchenOrder = kitchenOrderRepository.findByRef(kitchenOrderRef);
+		kitchenOrder.startPrep();
 	}
 
 	@Override
 	public void finishPizzaPrep(PizzaRef ref) {
+		Pizza pizza = pizzaRepository.findByRef(ref);
+		pizza.finishPrep();
 	}
 
 	@Override
 	public void removePizzaFromOven(PizzaRef ref) {
+		Pizza pizza = pizzaRepository.findByRef(ref);
+		pizza.finishBake();
 	}
 
 	@Override
 	public KitchenOrder findKitchenOrderByRef(KitchenOrderRef kitchenOrderRef) {
-		return null;
+		return kitchenOrderRepository.findByRef(kitchenOrderRef);
 	}
 
 	@Override
 	public KitchenOrder findKitchenOrderByOnlineOrderRef(OnlineOrderRef onlineOrderRef) {
-		return null;
+		return kitchenOrderRepository.findByOnlineOrderRef(onlineOrderRef);
 	}
 
 	@Override
 	public Pizza findPizzaByRef(PizzaRef ref) {
-		return null;
+		return pizzaRepository.findByRef(ref);
 	}
 
 	@Override
 	public Set<Pizza> findPizzasByKitchenOrderRef(KitchenOrderRef kitchenOrderRef) {
-		return null;
+		return pizzaRepository.findPizzasByKitchenOrderRef(kitchenOrderRef);
 	}
 
 }
